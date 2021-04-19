dd762e4bc157a4aa41cd845a4fac324b6172c15f
MathildeRoseKoch <mathildekoch1@gmail.com>
2021-04-19 14:34:42 +0200

    Update update.js

--- controllers/update.js
+++ controllers/update.js
@@ -1,4 +1,4 @@
-var form = document.getElementById("form")
+var form = document.getElementById("update")
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
@@ -8,7 +8,7 @@ form.addEventListener('submit', function(e) {
     var city = document.getElementById("city").value
     var birthdate = document.getElementById("birthdate").value
     var name = document.getElementById("name").value
-    var hashed_password = document.getElementById("hashed_password").value
+
 
     fetch("http://localhost:7071/api/HttpTrigger1test", {
         method: 'UPDATE',
@@ -18,7 +18,6 @@ form.addEventListener('submit', function(e) {
             city: city, 
             birthdate: birthdate,
             name: name,
-            hashed_password: hashed_password
         }), 
         headers: {
             "Content-Type": "application/json; charset-UTF-8"
