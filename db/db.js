9883051b1c8132d93cb4e9e4ef8a4d404512a8c7
AndreasHaavik <anha20am@student.cbs.dk>
2021-04-19 09:55:46 +0200

    Create db.js

---
+++ db/db.js
@@ -0,0 +1,70 @@
+const { Connection, Request, TYPES} = require('tedious');
+const config = require('./config.json')
+
+var connection = new Connection(config)
+
+function startDb(){
+    return new Promise((resolve, reject) => {
+        connection.on('connect', (err) => {
+            if (err) {
+                console.log("Connection failed")
+                reject(err)
+                throw err;
+            } else {
+                console.log("Connected")
+                resolve();
+            }
+        })
+        connection.connect();
+    })
+}
+
+module.exports.sqlConnection = connection;
+module.exports.startDb = startDb;
+
+function insert(payload){
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO Tinder2.0.user (email, gender, city, birthdate, name) VALUES (@email, @gender, @city, @birthdate, @name)`
+        const request = new Request(sql, (err) => {
+            if (err){
+                reject(err)
+                console.log(err)
+            }
+        });
+        request.addParameter('email', TYPES.VarChar, payload.email)
+        request.addParameter('gender', TYPES.VarChar, payload.gender)
+        request.addParameter('city', TYPES.VarChar, payload.city)
+        request.addParameter('birthdate', TYPES.VarChar, payload.birthdate)
+        request.addParameter('name', TYPES.Date, payload.name)
+
+        request.on('requestCompleted', (row) => {
+            console.log('User inserted', row);
+            resolve('user inserted', row)
+        });
+        connection.execSql(request)
+
+    });
+}
+module.exports.insert = insert;
+
+function select(name){
+    return new Promise((resolve, reject) => {
+        const sql = 'SELECT * FROM Tinder2.0.user where name = @name'
+        const request = new Request(sql, (err, rowcount) => {
+            if (err){
+                reject(err)
+                console.log(err)
+            } else if (rowcount == 0) {
+                reject({message: 'User does not exist'})
+            }
+        });
+        request.addParameter('name', TYPES.VarChar, name)
+    
+        request.on('row', (columns) => {
+            resolve(columns)
+        });
+        connection.execSql(request)
+    })
+
+}
+module.exports.select = select;
