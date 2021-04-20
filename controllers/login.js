1e6b3d98b38a9236034e69d80eb39514aa249be0
MathildeRoseKoch <mathildekoch1@gmail.com>
2021-04-20 14:41:53 +0200

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,27 +1,29 @@
-/*form = document.getElementById("login")
 
-        form.addEventListener("submit", function(e){
+var form = document.getElementById("login")
+
+        form.addEventListener('submit', function(e){
             e.preventDefault()
-        
-            var name = document.getElementById("name").value
             
-               
-            fetch(`http://localhost:7071/api/login`,  {
-                method: 'POST',
-                body: JSON.stringify({ 
-                    name: name
-                }), 
-                headers: {
-                    "Content-Type": "application/json; charset-UTF-8"
-                }
+            var city = document.getElementById("city").value
+            var name = document.getElementById("name").value
+                       
+        fetch("http://localhost:7071/api/login", {
+            method: 'POST',
+            body:JSON.stringify({ 
+                city: city,
+                name: name
+    
+        }), 
+            headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        },
             })
                 .then((response) => {
-                        return response.json()
-                    })
-                    .then((data) => {
-                        console.log(data)
+                    return response.json()
+                     })
+                .then((data) => {
+                    console.log(data)
                     }).catch((err) =>{
-                        console.log(err + "sille")
-                    })
-                })*/
-        
+                    console.log(err)
+                            });
+                        })      
