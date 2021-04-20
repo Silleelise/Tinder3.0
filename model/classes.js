4abb22a23109e8a90077de35a65021ea6a7277fb
AndreasHaavik <anha20am@student.cbs.dk>
2021-04-20 14:27:18 +0200

    Create classes.js

---
+++ model/classes.js
@@ -0,0 +1,9 @@
+module.exports = class User{
+    constructor(email,gender,city,birthdate,name){
+        this.email = email;
+        this.gender = gender;
+        this.city = city;
+        this.birthdate = birthdate;
+        this.name = name;
+    }
+}
