504c07dc471be8895bc69a5438c93e9a76cbf022
will613 <williamwedell26@gmail.com>
2021-04-21 11:50:12 +0200

    Update delete.js

--- controllers/delete.js
+++ controllers/delete.js
@@ -1,23 +1,28 @@
 var getButton = document.getElementById("deleteUsers")
 
 getButton.addEventListener("click", function(){
-    var name1 = document.getElementById("name").value
+    var email = document.getElementById("email").value
     
-    fetch(`http://localhost:7071/api/deleteuser, ${name1}`, {
+    fetch(`http://localhost:7071/api/deleteuser?email=${email}`, {
         method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+        body: JSON.stringify({ 
+          email: email}),
+          headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        }
       })
-      .then((response) => response.json())
-      //Then with the data from the response in JSON...
-      .then((data) => {
-        console.log('Success:', data);
-      })
-      //Then with the error genereted...
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-
-});
+      .then(
+        function(response){
+            if (response.status !== 200){
+                console.log("Noget gik galt" + response.status);
+                return;
+            }
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    )
+    .catch(function (err){
+        console.log(err);
+    });
+})
