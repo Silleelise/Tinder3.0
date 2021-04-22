808dfe72456658949ba4eef1045e207ea4b3dfec
Silleelise <72793339+Silleelise@users.noreply.github.com>
2021-04-22 13:44:08 +0200

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,30 +1,29 @@
+var form = document.getElementById("login");
 
-var form = document.getElementById("login")
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-        form.addEventListener('submit', function(e){
-            e.preventDefault()
-            
-            var email = document.getElementById("email").value
-            var hashed_password = document.getElementById("hashed_password").value
+  var email = document.getElementById("email").value;
+  var hashed_password = document.getElementById("hashed_password").value;
 
-        fetch("http://localhost:7071/api/login", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json; charset-UTF-8"    
-            },
-            body:JSON.stringify({ 
-                email: email,
-                hashed_password: hashed_password
-             }),
-        })
-                .then((response) => {
-                    return response.json()
-                     })
-                .then((data) => {
-                    console.log(data);
-                    window.location.href ="profil.html"
-
-                    }).catch((err) =>{
-                    console.log(err)
-                            })
-                        })  
+  fetch("http://localhost:7071/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset-UTF-8",
+    },
+    body: JSON.stringify({
+      email: email,
+      hashed_password: hashed_password,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      window.location.href = "profile.html";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
