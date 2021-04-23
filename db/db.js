41c1582a0f1567a038a19338403ffadbc8e93a3f
Silleelise <72793339+Silleelise@users.noreply.github.com>
2021-04-23 09:14:31 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -25,7 +25,7 @@ module.exports.startDb = startDb;
 
 function insert(payload){
     return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO [Tinder2.0].[user] (email, gender, city, birthdate, name, hashed_password) VALUES (@email, @gender, @city, @birthdate, @name, @hashed_password)`
+        const sql = `INSERT INTO [Tinder2.0].[user] (email, gender, city, birthdate, name, hashed_password, interest) VALUES (@email, @gender, @city, @birthdate, @name, @hashed_password, @interest)`
         const request = new Request(sql, (err) => {
             if (err){
                 reject(err)
@@ -38,6 +38,7 @@ function insert(payload){
         request.addParameter('birthdate', TYPES.Date, payload.birthdate)
         request.addParameter('name', TYPES.VarChar, payload.name)
         request.addParameter('hashed_password', TYPES.VarChar, payload.hashed_password)
+        request.addParameter('interest', TYPES.VarChar, payload.interest)
        
 
         request.on('requestCompleted', (row) => {
@@ -78,7 +79,8 @@ function update(payload){
         name = @name, 
         gender = @gender, 
         city = @city, 
-        birthdate = @birthdate
+        birthdate = @birthdate,
+        interest = @interest
         FROM [Tinder2.0].[user] as OI
         WHERE email = @email`
         const request = new Request(sql, (err) => {
@@ -93,6 +95,7 @@ function update(payload){
         request.addParameter('city', TYPES.VarChar, payload.city)
         request.addParameter('birthdate', TYPES.Date, payload.birthdate)
         request.addParameter('name', TYPES.VarChar, payload.name)
+        request.addParameter('interest', TYPES.VarChar, payload.interest)
     
             request.on('requestCompleted', (row) => {
             console.log('User has been updated', row);
