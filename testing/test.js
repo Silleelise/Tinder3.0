4af82e0c9332ad4a04dd350dd449e53993fe9915
Silleelise <72793339+Silleelise@users.noreply.github.com>
2021-04-24 15:22:25 +0200

    Create test.js

---
+++ testing/test.js
@@ -0,0 +1,34 @@
+/*const chai = require("chai");
+const { response } = require ('express');
+const expect = require ('chai').expect
+const fetch = require("node-fetch");
+const chaiHttp = require('chai-http')
+const should = chai.should();
+chai.use(chaiHttp)
+
+describe("test", () => {
+  //den tester for kode 200 OK
+  it("should test if the server is responsive, code 200", (done) => {
+    chai
+      .request("http://localhost:7071/api/HttpTrigger1test")
+      .get("/") //Startsiden (login)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});*/
+
+const { response } = require("express");
+const expect = require("chai").expect
+const fetch = require("node-fetch")
+
+describe("test af endpoint register", function(){
+    it('should return > html file <', function () {
+       fetch("http://localhost:7071/api/HttpTrigger1test").then (response => {
+            response.should.have.status(200)
+       })
+
+    });
+})
+
