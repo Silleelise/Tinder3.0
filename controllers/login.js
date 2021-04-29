87cafe1b9d5d3eb7ccb4de2e9409575917b4d017
MathildeRoseKoch <mathildekoch1@gmail.com>
2021-04-29 12:57:50 +0200

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -29,3 +29,33 @@ form.addEventListener("submit", function (e) {
     });
 });
 
+
+/*form.addEventListener("submit", function () {
+  e.preventDefault();
+
+  var email = document.getElementById("email").value;
+  var hashed_password = document.getElementById("hashed_password").value;
+
+  fetch("http://localhost:7071/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset-UTF-8",
+    },
+    body: JSON.stringify({
+      email: email,
+      hashed_password: hashed_password,
+    }),
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    sessionStorage.setItem('user',email);
+    window.location.href = "profile.html";
+    req.session.destroy();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});*/
