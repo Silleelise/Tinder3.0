1ef924f10b8a44a300afd7258e78e65ae7d8f6bd
Silleelise <72793339+Silleelise@users.noreply.github.com>
2021-04-30 15:18:02 +0200

    Update matches.js

---
+++ controllers/matches.js
@@ -0,0 +1,52 @@
+var form = document.getElementById("update")
+
+function isAuth(){
+    let email = sessionStorage.getItem('email');
+    if (email == null){
+        console.log("nu er vi nået til linje 6")
+        console.log(email)
+        return 
+    }else{
+        window.location.href = "login.html" 
+    }
+}
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    var email = document.getElementById("email").value
+    var gender = document.getElementById("gender").value
+    var region = document.getElementById("region").value
+    var age = document.getElementById("age").value
+    var name = document.getElementById("name").value
+    var interest = document.getElementById("interest").value
+
+
+    fetch(`http://localhost:7071/api/updateuser`, {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        },
+        body: JSON.stringify({
+            email: email,
+            gender: gender,
+            region: region, 
+            age: age,
+            name: name,
+            interest: interest
+        }), 
+       
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
+
+
+
