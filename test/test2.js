fb4bf1f5aa60b3e0f1953a658d008f54e8a528be
MathildeRoseKoch <mathildekoch1@gmail.com>
2021-05-08 16:58:31 +0200

    Update test2.js

--- test/test2.js
+++ test/test2.js
@@ -1,31 +1,7 @@
-/*const chaiHttp = require('chai-http');
-const server = 'http://localhost:7071/api/getusers';
-const { response } = require("express"); 
-const chai = require('chai')
-
-const should = require('should');
-chai.use(chaiHttp);
-
-describe('/GET book', () => {
-    it('it should GET all the books', (done) => {
-      chai.request(server)
-          .get('/book')
-          .end((err, response) => {
-              //response.should.have.status(200);
-              should.exist(response.body);
-              should(response.body).be.a('array');
-              response.body.length.should.be.eql(0);
-            done();
-          });
-    });
-});*/
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { response } = require('express');
 const server = 'http://localhost:7071/api/getusers';
-//const should = require('should');
-//const should = chai.should();
 
 chai.use(chaiHttp);
 
