efabe99364a008af9dd32254c12cb3d603e44306
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 00:49:14 +0200

    Update deleteuser.js

--- deleteuser/deleteuser.js
+++ deleteuser/deleteuser.js
@@ -1,3 +1,5 @@
+
+// get delete function from db, by using require method
 const db = require('../db/db');
 
 module.exports = async function (context, req) {
@@ -7,7 +9,7 @@ module.exports = async function (context, req) {
         await db.startDb(); //start db connection
     } catch (error) {
         console.log("Error connecting to the database", error.message)
-    }
+    } // define methods
     switch (req.method) {
         case 'GET':
             await get(context, req);
@@ -23,13 +25,18 @@ module.exports = async function (context, req) {
     }
 }
 
+// using DELETE method with async function
 async function DELETE(context, req){
+    // using try catch statement, to delete user 
     try{
+        //await on Promise from deleteUser function from the database (db.js) 
         let payload = req.body;
         await db.deleteUser(payload)
+        // response with status: 'Delete succes'
         context.res = {
             body: {status: 'Delete succes'}
         };
+        // cathing error if the try-statement dosen't work and fails
     } catch(error){
         context.res = {
             status: 400,
