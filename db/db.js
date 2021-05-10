08e21ff3932988ee97638e47bebed98e5b95a402
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-10 12:19:46 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const { Connection, Request, TYPES } = require("tedious");
 const config = require("./config.json");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); 
 
 var connection = new Connection(config);
 
