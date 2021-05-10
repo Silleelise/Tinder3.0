b7f4354174bda1afe004a630ee5e0b28c3013d86
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 00:49:26 +0200

    Update login_index.js

--- login/login_index.js
+++ login/login_index.js
@@ -1,3 +1,5 @@
+
+// get login function from db, by using require method
 const db = require('../db/db');
 
  module.exports = async function (context, req) {
@@ -5,17 +7,17 @@ const db = require('../db/db');
 
     
     try {
-    await db.startDb(); //start db connection
+        await db.startDb(); //start db connection
     } catch (error) {
-    console.log("Error connecting to the database", error.message)
-}
-switch (req.method) {
-    case 'GET':
-        await get(context, req);
-        break;
-    case 'POST':
-        await post(context, req);
-        break
+        console.log("Error connecting to the database", error.message)
+    }// define methods 
+    switch (req.method) {
+        case 'GET':
+            await get(context, req);
+            break;
+        case 'POST':
+            await post(context, req);
+            break
      default:
          context.res = {
         body: "please get or post"
@@ -24,14 +26,18 @@ switch (req.method) {
             break
 }
 
-
+// using post method with async function
 async function post(context, req){
+    // using try catch statement, to login for a user 
     try{
         let payload = req.body
+        //await on Promise from login-function from the database (db.js)
         await db.login(payload)
+         // reponse with a object 
         context.res = {
             body: {status: 'Succes with logging in'}
         }
+        // cathing error if the try-statement dosen't work and fails
     } catch(error){
         context.res = {
             status: 400,
