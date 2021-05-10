acc62fa14a60e4a99b84e9140b5a46ee0679c36e
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 00:49:28 +0200

    Update updateuser.js

--- updateuser/updateuser.js
+++ updateuser/updateuser.js
@@ -1,5 +1,7 @@
+
+// get update function from db, by using require method
 const db = require('../db/db');
-//const User = require("../model/classes");
+
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.')
@@ -8,7 +10,7 @@ module.exports = async function (context, req) {
         await db.startDb(); //start db connection
     } catch (error) {
         console.log("Error connecting to the database", error.message)
-    }
+    } // define methods 
     switch (req.method) {
         case 'GET':
             await get(context, req);
@@ -27,14 +29,18 @@ module.exports = async function (context, req) {
     }
 }
 
+// using patch method with async function
 async function patch(context, req){
+    // using try catch statement, to update user 
     try{
-        //let payload = new User(req.body.email, req.body.gender, req.body.city, req.body.birthdate, req.body.name)
+        //await on Promise from update function from the database (db.js) 
         let payload = req.body
         await db.update(payload)
+        // reponse with a object 
         context.res = {
             body: {status: 'Update succes'}
         }
+        // cathing error if the try-statement dosen't work and fails
     } catch(error){
         context.res = {
             status: 400,
