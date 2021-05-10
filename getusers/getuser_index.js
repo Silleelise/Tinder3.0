5609b90088381aa1a68141898e40561e1910a454
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 00:49:21 +0200

    Update getuser_index.js

--- getusers/getuser_index.js
+++ getusers/getuser_index.js
@@ -1,18 +1,25 @@
+// using tedious to make Connection and Request 
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request
 
+// get access to database through microsoft azure
 const config = require('../db/config.json')
 
+// make function with a new connection and new request 
 const executeSQL = (context, user) => {
     var result = '';
 
     const connection = new Connection(config);
     
+    // make SQL-query 
     const request = new Request(` SELECT COUNT(*) FROM [Tinder2.0].[user] `, function(err){
+        // if else satement,
+        // i statment handle error
         if (err){
             context.log.error(err);
             context.res.status = 500;
             context.res.body = 'Error executing T-SQL command';
+            // else stament handle response
         } else {
             context.res = {
                 body: result
@@ -20,13 +27,15 @@ const executeSQL = (context, user) => {
         }
         context.done();
     });
-
+    // another if else statment inside connection.on
     connection.on('connect', function(err){
+        // if error = handle error 
         if (err){
             context.log.error(err);
             context.res.status = 500;
             context.res.body = 'Error connecting to Azure';
             context.done();
+            // else handle connection from function execSql (line 9), with request as a argument (line 15)
         } else {
             connection.execSql(request)
         }
