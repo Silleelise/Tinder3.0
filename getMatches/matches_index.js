fc63042f34546bd9215465ad50b400f8abc1e503
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 00:49:17 +0200

    Update matches_index.js

--- getMatches/matches_index.js
+++ getMatches/matches_index.js
@@ -1,3 +1,5 @@
+
+// get matches-function from db, by using require method
 const db = require('../db/db');
 
 module.exports = async function (context, req) {
@@ -7,7 +9,7 @@ module.exports = async function (context, req) {
         await db.startDb(); //start db connection
     } catch (error) {
         console.log("Error connecting to the database", error.message)
-    }
+    }// define methods 
     switch (req.method) {
         case 'GET':
             await get(context, req);
@@ -22,26 +24,33 @@ module.exports = async function (context, req) {
             break
     }
 }
-
+// using post method with async function
 async function post(context, req){
+    // make try catch statment 
     try{
         let payload = req.body
+        // await on Promise from matches function from the database (db.js)
         let users = await db.matches(payload)
-        // console.log(users[0])
+        // make empty array
         let all = []
+        // make the first loop, with an empty object
         users.forEach(function(u){
             response = {}
+        // make next loop that loops throug user.metadata.colname
             u.forEach(function(user){
                 console.log(user.value)
                 response[user.metadata.colName] = user.value 
                 console.log(user.metadata.colName)
-                // reponse = users.value
             });
+        // push response to the empty array called "all", watch line 35
             all.push(response)
         })
+        // make context.res with the body object all
+        // inside the array "all", is where all the users are from the database 
             context.res = {
                 body: all
             }
+            // if there is an error it will make a response 400
     } catch(error){
         context.res = {
             status: 400,
