868390542277fda4e4113d37f7fe065eb4f491e1
AndreasHaavik <anha20am@student.cbs.dk>
2021-05-11 11:54:31 +0200

    Update logout.js

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,19 +1,19 @@
+
+
 var button = document.getElementById("logout")
 
+// this function makes it possible to log out, by using removeItem method 
 function logout(){
     let email = sessionStorage.removeItem('user');
     if (email == 'email'){
+        // redirect to profile if the email == 'email'
         window.location.href = "profile.html" 
         console.log(email)
         return 
     }else{
+        // else logout and redirect to login.html
         window.location.href = "login.html" 
     }
 }
 
 
-/*
-button.addEventListener('click', function(e)) {
-
-}
-*/
